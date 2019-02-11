bb525dd1782229082a1df4778d6b789cad0b8857
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-02-11 16:31:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   # code goes here
   attr_reader :species
-  attr_accessor :name
+  attr_accessor :name :pets
   @@all = Array.new
 
   def initialize(species)
@@ -27,5 +27,9 @@ class Owner
 
   def say_species
     "I am a #{self.species}."
+  end
+
+  def pets
+    
   end
 end
