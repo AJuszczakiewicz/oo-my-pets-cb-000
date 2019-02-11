85a6e0d96f818096c43265a953788edda4edadba
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-02-11 16:15:35 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,5 +1,18 @@
-class Fish
+class Pet
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+end
+
+
+class Fish < Pet
   def initialize
     super
   end
 end
+
+
