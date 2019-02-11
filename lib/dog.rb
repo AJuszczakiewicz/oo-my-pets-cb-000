d070079275ecd71685193feb75ab8a6c95bc3ea1
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-02-11 16:20:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,4 @@
 require_relative './pet'
-class Pet
-  attr_reader :name
-  attr_accessor :mood
-
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-end
-
 
 class Dog < Pet
   # code goes here
